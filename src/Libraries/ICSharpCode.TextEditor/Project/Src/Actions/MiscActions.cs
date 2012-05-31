nylen/SharpDevelop

commit 29b3c5157d7d20d87ac9a2c8ae3a8f1f0b18b073

Improve algorithm used for manual indenting/unindenting
The new logic is in ICSharpCode.TextEditor.Util.IndentHelper, and
Tab.GetIndentationString() has also been moved to IndentHelper.

The new algorithm has the following improvements over the old one:
 - If a line is partially indented ("in between" indentation levels),
   indent/unindent to the nearest full level
 - Preserve caret location and selection boundaries when
   indenting/unindenting
 - When the caret is inside of a line's leading whitespace, Backspace
   will decrease the indent level using the same logic.
 - Pressing Tab or Shift+Tab when nothing is selected will indent or
   unindent the current line by one level.

src/Libraries/ICSharpCode.TextEditor/Project/Src/Actions/MiscActions.cs
@@ -11,68 +11,12 @@ using System.Drawing;
 using System.Text;
 
 using ICSharpCode.TextEditor.Document;
+using ICSharpCode.TextEditor.Util;
 
 namespace ICSharpCode.TextEditor.Actions
 {
 	public class Tab : AbstractEditAction
 	{
-		public static string GetIndentationString(IDocument document)
-		{
-			return GetIndentationString(document, null);
-		}
-		
-		public static string GetIndentationString(IDocument document, TextArea textArea)
-		{
-			StringBuilder indent = new StringBuilder();
-			
-			if (document.TextEditorProperties.ConvertTabsToSpaces) {
-				int tabIndent = document.TextEditorProperties.IndentationSize;
-				if (textArea != null) {
-					int column = textArea.TextView.GetVisualColumn(textArea.Caret.Line, textArea.Caret.Column);
-					indent.Append(new String(' ', tabIndent - column % tabIndent));
-				} else {
-					indent.Append(new String(' ', tabIndent));
-				}
-			} else {
-				indent.Append('\t');
-			}
-			return indent.ToString();
-		}
-		
-		void InsertTabs(IDocument document, ISelection selection, int y1, int y2)
-		{
-			string indentationString = GetIndentationString(document);
-			for (int i = y2; i >= y1; --i) {
-				LineSegment line = document.GetLineSegment(i);
-				if (i == y2 && i == selection.EndPosition.Y && selection.EndPosition.X  == 0) {
-					continue;
-				}
-				
-				// this bit is optional - but useful if you are using block tabbing to sort out
-				// a source file with a mixture of tabs and spaces
-//				string newLine = document.GetText(line.Offset,line.Length);
-//				document.Replace(line.Offset,line.Length,newLine);
-				
-				document.Insert(line.Offset, indentationString);
-			}
-		}
-		
-		void InsertTabAtCaretPosition(TextArea textArea)
-		{
-			switch (textArea.Caret.CaretMode) {
-				case CaretMode.InsertMode:
-					textArea.InsertString(GetIndentationString(textArea.Document, textArea));
-					break;
-				case CaretMode.OverwriteMode:
-					string indentStr = GetIndentationString(textArea.Document, textArea);
-					textArea.ReplaceChar(indentStr[0]);
-					if (indentStr.Length > 1) {
-						textArea.InsertString(indentStr.Substring(1));
-					}
-					break;
-			}
-			textArea.SetDesiredColumn();
-		}
 		/// <remarks>
 		/// Executes this edit action
 		/// </remarks>
@@ -82,25 +26,38 @@ namespace ICSharpCode.TextEditor.Actions
 			if (textArea.SelectionManager.SelectionIsReadonly) {
 				return;
 			}
+			IndentHelper indentHelper = new IndentHelper(textArea.Document);
 			textArea.Document.UndoStack.StartUndoGroup();
+			
 			if (textArea.SelectionManager.HasSomethingSelected) {
 				foreach (ISelection selection in textArea.SelectionManager.SelectionCollection) {
 					int startLine = selection.StartPosition.Y;
 					int endLine   = selection.EndPosition.Y;
-					if (startLine != endLine) {
-						textArea.BeginUpdate();
-						InsertTabs(textArea.Document, selection, startLine, endLine);
-						textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.LinesBetween, startLine, endLine));
-						textArea.EndUpdate();
-					} else {
-						InsertTabAtCaretPosition(textArea);
-						break;
+					textArea.BeginUpdate();
+					for (int lineNum = startLine; lineNum <= endLine; lineNum++) {
+						IndentOperationInfo info = indentHelper.IndentLine(lineNum);
+						if (lineNum == textArea.Caret.Position.Line) {
+							textArea.Caret.Position = info.AdjustTextLocation(textArea.Caret.Position);
+						}
+						if (lineNum == startLine) {
+							selection.StartPosition = info.AdjustTextLocation(selection.StartPosition);
+						}
+						if (lineNum == endLine) {
+							selection.EndPosition = info.AdjustTextLocation(selection.EndPosition);
+						}
 					}
+					textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.LinesBetween, startLine, endLine));
+					textArea.EndUpdate();
 				}
 				textArea.Document.CommitUpdate();
 				textArea.AutoClearSelection = false;
 			} else {
-				InsertTabAtCaretPosition(textArea);
+				// Pressing Tab with nothing selected will indent the current line by one level.
+				int currentLine = textArea.Document.GetLineNumberForOffset(textArea.Caret.Offset);
+				IndentOperationInfo info = indentHelper.IndentLine(currentLine);
+				textArea.Caret.Position = info.AdjustTextLocation(textArea.Caret.Position);
+				textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.SingleLine, currentLine));
+				textArea.Document.CommitUpdate();
 			}
 			textArea.Document.UndoStack.EndUndoGroup();
 		}
@@ -108,103 +65,49 @@ namespace ICSharpCode.TextEditor.Actions
 	
 	public class ShiftTab : AbstractEditAction
 	{
-		void RemoveTabs(IDocument document, ISelection selection, int y1, int y2)
-		{
-			document.UndoStack.StartUndoGroup();
-			for (int i = y2; i >= y1; --i) {
-				LineSegment line = document.GetLineSegment(i);
-				if (i == y2 && line.Offset == selection.EndOffset) {
-					continue;
-				}
-				if (line.Length > 0) {
-					/**** TextPad Strategy:
-					/// first convert leading whitespace to tabs (controversial! - not all editors work like this)
-					string newLine = TextUtilities.LeadingWhiteSpaceToTabs(document.GetText(line.Offset,line.Length),document.Properties.Get("TabIndent", 4));
-					if(newLine.Length > 0 && newLine[0] == '\t') {
-						document.Replace(line.Offset,line.Length,newLine.Substring(1));
-						++redocounter;
-					}
-					else if(newLine.Length > 0 && newLine[0] == ' ') {
-						/// there were just some leading spaces but less than TabIndent of them
-						int leadingSpaces = 1;
-						for(leadingSpaces = 1; leadingSpaces < newLine.Length && newLine[leadingSpaces] == ' '; leadingSpaces++) {
-							/// deliberately empty
-						}
-						document.Replace(line.Offset,line.Length,newLine.Substring(leadingSpaces));
-						++redocounter;
-					}
-					/// else
-					/// there were no leading tabs or spaces on this line so do nothing
-					/// MS Visual Studio 6 strategy:
-					 ****/
-//					string temp = document.GetText(line.Offset,line.Length);
-					if (line.Length > 0) {
-						int charactersToRemove = 0;
-						if(document.GetCharAt(line.Offset) == '\t') { // first character is a tab - just remove it
-							charactersToRemove = 1;
-						} else if(document.GetCharAt(line.Offset) == ' ') {
-							int leadingSpaces = 1;
-							int tabIndent = document.TextEditorProperties.IndentationSize;
-							for (leadingSpaces = 1; leadingSpaces < line.Length && document.GetCharAt(line.Offset + leadingSpaces) == ' '; leadingSpaces++) {
-								// deliberately empty
-							}
-							if(leadingSpaces >= tabIndent) {
-								// just remove tabIndent
-								charactersToRemove = tabIndent;
-							}
-							else if(line.Length > leadingSpaces && document.GetCharAt(line.Offset + leadingSpaces) == '\t') {
-								// remove the leading spaces and the following tab as they add up
-								// to just one tab stop
-								charactersToRemove = leadingSpaces+1;
-							}
-							else {
-								// just remove the leading spaces
-								charactersToRemove = leadingSpaces;
-							}
-						}
-						if (charactersToRemove > 0) {
-							document.Remove(line.Offset,charactersToRemove);
-						}
-					}
-				}
-			}
-			document.UndoStack.EndUndoGroup();
-		}
-		
 		/// <remarks>
 		/// Executes this edit action
 		/// </remarks>
 		/// <param name="textArea">The <see cref="ItextArea"/> which is used for callback purposes</param>
 		public override void Execute(TextArea textArea)
 		{
+			if (textArea.SelectionManager.SelectionIsReadonly) {
+				return;
+			}
+			IndentHelper indentHelper = new IndentHelper(textArea.Document);
+			textArea.Document.UndoStack.StartUndoGroup();
+			
 			if (textArea.SelectionManager.HasSomethingSelected) {
 				foreach (ISelection selection in textArea.SelectionManager.SelectionCollection) {
 					int startLine = selection.StartPosition.Y;
 					int endLine   = selection.EndPosition.Y;
 					textArea.BeginUpdate();
-					RemoveTabs(textArea.Document, selection, startLine, endLine);
+					for (int lineNum = startLine; lineNum <= endLine; lineNum++) {
+						IndentOperationInfo info = indentHelper.UnIndentLine(lineNum);
+						if (lineNum == textArea.Caret.Position.Line) {
+							textArea.Caret.Position = info.AdjustTextLocation(textArea.Caret.Position);
+						}
+						if (lineNum == startLine) {
+							selection.StartPosition = info.AdjustTextLocation(selection.StartPosition);
+						}
+						if (lineNum == endLine) {
+							selection.EndPosition = info.AdjustTextLocation(selection.EndPosition);
+						}
+					}
 					textArea.Document.UpdateQueue.Clear();
 					textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.LinesBetween, startLine, endLine));
 					textArea.EndUpdate();
-					
 				}
 				textArea.AutoClearSelection = false;
 			} else {
-				// Pressing Shift-Tab with nothing selected the cursor will move back to the
-				// previous tab stop. It will stop at the beginning of the line. Also, the desired
-				// column is updated to that column.
-				LineSegment line = textArea.Document.GetLineSegmentForOffset(textArea.Caret.Offset);
-				string startOfLine = textArea.Document.GetText(line.Offset,textArea.Caret.Offset - line.Offset);
-				int tabIndent = textArea.Document.TextEditorProperties.IndentationSize;
-				int currentColumn = textArea.Caret.Column;
-				int remainder = currentColumn % tabIndent;
-				if (remainder == 0) {
-					textArea.Caret.DesiredColumn = Math.Max(0, currentColumn - tabIndent);
-				} else {
-					textArea.Caret.DesiredColumn = Math.Max(0, currentColumn - remainder);
-				}
-				textArea.SetCaretToDesiredColumn();
+				// Pressing Shift-Tab with nothing selected will un-indent the current line by one level.
+				int currentLine = textArea.Document.GetLineNumberForOffset(textArea.Caret.Offset);
+				IndentOperationInfo info = indentHelper.UnIndentLine(currentLine);
+				textArea.Caret.Position = info.AdjustTextLocation(textArea.Caret.Position);
+				textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.SingleLine, currentLine));
+				textArea.Document.CommitUpdate();
 			}
+			textArea.Document.UndoStack.EndUndoGroup();
 		}
 	}
 	
@@ -566,11 +469,21 @@ namespace ICSharpCode.TextEditor.Actions
 						textArea.Caret.Position = new TextLocation(lineLength, curLineNr - 1);
 						textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.PositionToEnd, new TextLocation(0, curLineNr - 1)));
 					} else {
-						int caretOffset = textArea.Caret.Offset - 1;
-						textArea.Caret.Position = textArea.Document.OffsetToPosition(caretOffset);
-						textArea.Document.Remove(caretOffset, 1);
+						string lineTextToCaret = textArea.Document.GetText(curLineOffset, textArea.Caret.Offset - curLineOffset);
+						TextLocation caretPos = textArea.Caret.Position;
+						if (lineTextToCaret.Trim() == "") {
+							// If we're inside of a line's leading whitespace, Backspace will decrease
+							// the line's indentation level.
+							IndentHelper indentHelper = new IndentHelper(textArea.Document);
+							caretPos = indentHelper.UnIndentLine(curLineNr).AdjustTextLocation(caretPos);
+						} else {
+							caretPos.Column--;
+							textArea.Document.Remove(textArea.Caret.Offset - 1, 1);
+						}
+						textArea.Caret.Position = caretPos;
 						
-						textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.PositionToLineEnd, new TextLocation(textArea.Caret.Offset - textArea.Document.GetLineSegment(curLineNr).Offset, curLineNr)));
+						textArea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.PositionToLineEnd,
+							new TextLocation(textArea.Caret.Offset - textArea.Document.GetLineSegment(curLineNr).Offset, curLineNr)));
 					}
 					textArea.EndUpdate();
 				}

src/Libraries/ICSharpCode.TextEditor/Project/Src/Util/IndentHelper.cs
@@ -1,0 +1,129 @@
+﻿using System;
+using ICSharpCode.TextEditor.Document;
+
+namespace ICSharpCode.TextEditor.Util {
+    public class IndentOperationInfo {
+        public int CurrentIndentWidth;
+        public int LineOffset;
+        public int StartOffset;
+        public int FirstNonSpaceOffset;
+        public int CharsRemoved;
+        public int CharsInserted;
+        public int IndentWidth;
+        
+        public int StartColumn {
+            get {
+                return StartOffset - LineOffset;
+            }
+        }
+        
+        public IndentOperationInfo(IDocument document, LineSegment line) {
+            IndentWidth = document.TextEditorProperties.IndentationSize;
+            CurrentIndentWidth = 0;
+            
+            FirstNonSpaceOffset = LineOffset = line.Offset;
+            char c;
+            while (IsIndentChar(c = document.GetCharAt(FirstNonSpaceOffset))) {
+                FirstNonSpaceOffset++;
+                switch (c) {
+                    case ' ':
+                        CurrentIndentWidth++;
+                        break;
+                    case '\t':
+                        CurrentIndentWidth += (IndentWidth - (CurrentIndentWidth % IndentWidth));
+                        break;
+                }
+            }
+        }
+        
+        public static bool IsIndentChar(char c) {
+            return (c == ' ' || c == '\t');
+        }
+        
+        public TextLocation AdjustTextLocation(TextLocation position) {
+            // Make the text position stay at the same place in the line after the indentation
+            // adjustment, regardless of where it is now.
+            if (position.Column >= StartColumn) {
+                position.Column -= Math.Min(position.Column - StartColumn, CharsRemoved);
+                position.Column += CharsInserted;
+            }
+            return position;
+        }
+    }
+    
+    public class IndentHelper {
+        private IDocument document;
+        
+        private int tabIndent {
+            get {
+                return document.TextEditorProperties.IndentationSize;
+            }
+        }
+        
+        public IndentHelper(IDocument document) {
+            this.document = document;
+        }
+        
+        public IndentOperationInfo UnIndentLine(int lineNum) {
+            LineSegment line = document.GetLineSegment(lineNum);
+            IndentOperationInfo info = new IndentOperationInfo(document, line);
+            info.StartOffset = info.FirstNonSpaceOffset;
+            
+            if (line.Length > 0) {
+                if (info.CurrentIndentWidth > 0) {
+                    if ((info.CurrentIndentWidth % info.IndentWidth) == 0) {
+                        // Remove a full indentation level from the end of the line's leading
+                        // whitespace.  This could be any of the following:
+                        //  - One tab + (n < tabIndent) spaces
+                        //  - One tab
+                        //  - tabIndent spaces
+                        while (info.CharsRemoved < info.IndentWidth) {
+                            info.CharsRemoved++;
+                            info.StartOffset--;
+                            if (document.GetCharAt(info.StartOffset) == '\t') {
+                                break;
+                            }
+                        }
+                    } else {
+                        // The current line is indented "in between" indentation levels, which
+                        // means we just need to remove (currentIndent % tabIndent) spaces.
+                        info.CharsRemoved = (info.CurrentIndentWidth % info.IndentWidth);
+                        info.StartOffset -= info.CharsRemoved;
+                    }
+                    if (info.CharsRemoved > 0) {
+                        // Remove the character(s) from the end of the line's leading whitespace.
+                        document.Remove(info.StartOffset, info.CharsRemoved);
+                    }
+                }
+            }
+            
+            return info;
+        }
+        
+        public IndentOperationInfo IndentLine(int lineNum) {
+            LineSegment line = document.GetLineSegment(lineNum);
+            IndentOperationInfo info = new IndentOperationInfo(document, line);
+            info.StartOffset = info.FirstNonSpaceOffset;
+            
+            if ((info.CurrentIndentWidth % info.IndentWidth) != 0) {
+                // Remove partial indentation from end of line
+                IndentOperationInfo temp = UnIndentLine(lineNum);
+                info.StartOffset = temp.StartOffset;
+                info.CharsInserted -= temp.CharsRemoved;
+            }
+            string indentString = GetIndentationString(document);
+            document.Insert(info.StartOffset, indentString);
+            info.CharsInserted += indentString.Length;
+            
+            return info;
+        }
+        
+        public static string GetIndentationString(IDocument document) {
+            if (document.TextEditorProperties.ConvertTabsToSpaces) {
+                return new string(' ', document.TextEditorProperties.IndentationSize);
+            } else {
+                return "\t";
+            }
+        }
+    }
+}

src/AddIns/BackendBindings/Boo/BooBinding/Project/Src/FormattingStrategy.cs
@@ -7,8 +7,8 @@
 
 using System;
 using ICSharpCode.TextEditor;
-using ICSharpCode.TextEditor.Actions;
 using ICSharpCode.TextEditor.Document;
+using ICSharpCode.TextEditor.Util;
 using System.Collections.Generic;
 
 namespace Grunwald.BooBinding
@@ -23,7 +23,7 @@ namespace Grunwald.BooBinding
 			if (document.GetText(previousLine).EndsWith(":")) {
 				LineSegment currentLine = document.GetLineSegment(line);
 				string indentation = GetIndentation(area, line-1);
-				indentation += Tab.GetIndentationString(document);
+				indentation += IndentHelper.GetIndentationString(document);
 				document.Replace(currentLine.Offset,
 				                 currentLine.Length,
 				                 indentation + document.GetText(currentLine));

src/AddIns/BackendBindings/CSharpBinding/Project/Src/FormattingStrategy/CSharpFormattingStrategy.cs
@@ -14,8 +14,8 @@ using System.Text;
 using ICSharpCode.SharpDevelop;
 using ICSharpCode.SharpDevelop.Dom;
 using ICSharpCode.TextEditor;
-using ICSharpCode.TextEditor.Actions;
 using ICSharpCode.TextEditor.Document;
+using ICSharpCode.TextEditor.Util;
 
 namespace CSharpBinding.FormattingStrategy
 {
@@ -44,7 +44,7 @@ namespace CSharpBinding.FormattingStrategy
 			DocumentAccessor acc = new DocumentAccessor(textArea.Document, lineNr, lineNr);
 			
 			IndentationSettings set = new IndentationSettings();
-			set.IndentString = Tab.GetIndentationString(textArea.Document);
+			set.IndentString = IndentHelper.GetIndentationString(textArea.Document);
 			set.LeaveEmptyLines = false;
 			IndentationReformatter r = new IndentationReformatter();
 			
@@ -82,7 +82,7 @@ namespace CSharpBinding.FormattingStrategy
 				oldIndentLength = GetIndentation(textArea, cursorPos).Length;
 			
 			IndentationSettings set = new IndentationSettings();
-			set.IndentString = Tab.GetIndentationString(textArea.Document);
+			set.IndentString = IndentHelper.GetIndentationString(textArea.Document);
 			IndentationReformatter r = new IndentationReformatter();
 			DocumentAccessor acc = new DocumentAccessor(textArea.Document, begin, end);
 			r.Reformat(acc, set);

src/AddIns/BackendBindings/Python/PythonBinding/Project/Src/PythonFormattingStrategy.cs
@@ -7,8 +7,8 @@
 
 using System;
 using ICSharpCode.TextEditor;
-using ICSharpCode.TextEditor.Actions;
 using ICSharpCode.TextEditor.Document;
+using ICSharpCode.TextEditor.Util;
 
 namespace ICSharpCode.PythonBinding
 {
@@ -57,7 +57,7 @@ namespace ICSharpCode.PythonBinding
 			IDocument document = textArea.Document;
 			LineSegment currentLine = document.GetLineSegment(line);
 			string indentation = GetIndentation(textArea, line - 1);
-			indentation = GetNewLineIndentation(indentation, Tab.GetIndentationString(document), increaseIndent);
+			indentation = GetNewLineIndentation(indentation, IndentHelper.GetIndentationString(document), increaseIndent);
 			string newIndentedText = indentation + document.GetText(currentLine);
 			SmartReplaceLine(document, currentLine, newIndentedText);
 			return indentation.Length;

src/AddIns/BackendBindings/Ruby/RubyBinding/Project/Src/RubyFormattingStrategy.cs
@@ -8,8 +8,8 @@
 using System;
 using System.Collections.Generic;
 using ICSharpCode.TextEditor;
-using ICSharpCode.TextEditor.Actions;
 using ICSharpCode.TextEditor.Document;
+using ICSharpCode.TextEditor.Util;
 
 namespace ICSharpCode.RubyBinding
 {
@@ -174,7 +174,7 @@ namespace ICSharpCode.RubyBinding
 		{
 			LineSegment currentLine = document.GetLineSegment(line);
 			string indentation = GetIndentation(textArea, line - 1);
-			indentation = GetNewLineIndentation(indentation, Tab.GetIndentationString(document), increaseIndent);
+			indentation = GetNewLineIndentation(indentation, IndentHelper.GetIndentationString(document), increaseIndent);
 			string newIndentedText = indentation + document.GetText(currentLine);
 			SmartReplaceLine(document, currentLine, newIndentedText);
 			return indentation.Length;

src/AddIns/BackendBindings/VBNetBinding/Project/Src/FormattingStrategy/VBNetFormattingStrategy.cs
@@ -20,8 +20,8 @@ using ICSharpCode.NRefactory.Parser.VB;
 using ICSharpCode.SharpDevelop;
 using ICSharpCode.SharpDevelop.Dom;
 using ICSharpCode.TextEditor;
-using ICSharpCode.TextEditor.Actions;
 using ICSharpCode.TextEditor.Document;
+using ICSharpCode.TextEditor.Util;
 
 namespace VBNetBinding.FormattingStrategy
 {
@@ -235,7 +235,7 @@ namespace VBNetBinding.FormattingStrategy
 									textArea.Document.Replace(curLine.Offset, curLine.Length, terminator + indentation + statement.EndStatement);
 								if (!IsInsideInterface(textArea, lineNr) || statement == interfaceStatement) {
 									for (int i = 0; i < statement.IndentPlus; i++) {
-										indentation += Tab.GetIndentationString(textArea.Document);
+										indentation += IndentHelper.GetIndentationString(textArea.Document);
 									}
 								}
 

src/Libraries/ICSharpCode.TextEditor/Project/ICSharpCode.TextEditor.csproj
@@ -91,6 +91,7 @@
     <Compile Include="Src\Document\TextBufferStrategy\StringTextBufferStrategy.cs" />
     <Compile Include="Src\Util\AugmentableRedBlackTree.cs" />
     <Compile Include="Src\Util\CheckedList.cs" />
+    <Compile Include="Src\Util\IndentHelper.cs" />
     <Compile Include="Src\Util\LoggingService.cs" />
     <Compile Include="Src\Util\LookupTable.cs" />
     <Compile Include="Src\Util\MouseWheelHandler.cs" />

commit 1818bb231974cdfeed28c676c2ea335ad08cbdfb

Fix some bugs introduced by commit 29b3c51
 - Don't crash when pressing Shift+Tab/Tab on last line of document
 - Handle indenting/unindenting whole lines properly (when the caret is
   on the line below the selection)

src/Libraries/ICSharpCode.TextEditor/Project/Src/Actions/MiscActions.cs
@@ -31,8 +31,11 @@ namespace ICSharpCode.TextEditor.Actions
 			
 			if (textArea.SelectionManager.HasSomethingSelected) {
 				foreach (ISelection selection in textArea.SelectionManager.SelectionCollection) {
-					int startLine = selection.StartPosition.Y;
-					int endLine   = selection.EndPosition.Y;
+					int startLine = selection.StartPosition.Line;
+					int endLine   = selection.EndPosition.Line;
+					if (selection.EndPosition.Column == 0) {
+					    endLine--;
+					}
 					textArea.BeginUpdate();
 					for (int lineNum = startLine; lineNum <= endLine; lineNum++) {
 						IndentOperationInfo info = indentHelper.IndentLine(lineNum);
@@ -79,8 +82,11 @@ namespace ICSharpCode.TextEditor.Actions
 			
 			if (textArea.SelectionManager.HasSomethingSelected) {
 				foreach (ISelection selection in textArea.SelectionManager.SelectionCollection) {
-					int startLine = selection.StartPosition.Y;
-					int endLine   = selection.EndPosition.Y;
+					int startLine = selection.StartPosition.Line;
+					int endLine   = selection.EndPosition.Line;
+					if (selection.EndPosition.Column == 0) {
+					    endLine--;
+					}
 					textArea.BeginUpdate();
 					for (int lineNum = startLine; lineNum <= endLine; lineNum++) {
 						IndentOperationInfo info = indentHelper.UnIndentLine(lineNum);

src/Libraries/ICSharpCode.TextEditor/Project/Src/Util/IndentHelper.cs
@@ -23,7 +23,9 @@ namespace ICSharpCode.TextEditor.Util {
             
             FirstNonSpaceOffset = LineOffset = line.Offset;
             char c;
-            while (IsIndentChar(c = document.GetCharAt(FirstNonSpaceOffset))) {
+            while (FirstNonSpaceOffset < document.TextLength
+                && IsIndentChar(c = document.GetCharAt(FirstNonSpaceOffset))) {
+                
                 FirstNonSpaceOffset++;
                 switch (c) {
                     case ' ':